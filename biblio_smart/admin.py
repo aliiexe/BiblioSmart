71360f9ac23770dd4602b5244c762ca5c90f3194
aliiexe <alibusinessbourak@gmail.com>
2025-04-06 01:06:40 +0000

    fixed the database bug

--- biblio_smart/admin.py
+++ biblio_smart/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Utilisateur, Livre, Lecteur, Bibliothecaire, Emprunt, Amende, Rapport, Evenement
 
-# Register your models here.
+admin.site.register(Utilisateur)
+admin.site.register(Livre)
+admin.site.register(Lecteur)
+admin.site.register(Bibliothecaire)
+admin.site.register(Emprunt)
+admin.site.register(Amende)
+admin.site.register(Rapport)
+admin.site.register(Evenement)
