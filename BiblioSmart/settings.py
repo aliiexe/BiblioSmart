c43b358230f38be26dd3aec1acba339005319f71
aliiexe <alibusinessbourak@gmail.com>
2025-05-07 00:56:22 +0100

    updated the landing page and some other pages styles and corrected the fines processing after login

--- BiblioSmart/settings.py
+++ BiblioSmart/settings.py
@@ -88,16 +88,6 @@ WSGI_APPLICATION = 'BiblioSmart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 # CREATE DATABASE biblio_smart_db;
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'biblio_smart_db', 
-        'USER': 'root', 
-        'PASSWORD':'Ali10062005bourak@',
-        'HOST': 'localhost',  
-        'PORT': '3306',   
-    }
-}
 
 # DATABASES = {
 #     'default': {
