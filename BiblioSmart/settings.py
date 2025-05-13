865a71e8e7359903c3ef6f8b28a2a8781ec91a57
Ali Bourak <130255094+aliiexe@users.noreply.github.com>
2025-05-13 16:54:49 +0100

    Update settings.py

--- BiblioSmart/settings.py
+++ BiblioSmart/settings.py
@@ -101,31 +101,18 @@ WSGI_APPLICATION = 'BiblioSmart.wsgi.application'
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'biblio_smart_db',
-#         'USER': '2hadRfj9ujbyhJw.root',
-#         'PASSWORD': 'sGryvKTjGw8EHkvr',
-#         'HOST': 'gateway01.eu-central-1.prod.aws.tidbcloud.com',
-#         'PORT': '4000',
-#         'OPTIONS': {
-#             'ssl_mode': 'REQUIRED',
-#             'charset': 'utf8mb4',
-#         }
-#     }
-# }
-
-# settings.py
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQLDATABASE', 'railway'),
-        'USER': os.environ.get('MYSQLUSER', 'root'),
-        'PASSWORD': os.environ.get('MYSQLPASSWORD', ''),
-        'HOST': os.environ.get('MYSQLHOST', 'localhost'),
-        'PORT': os.environ.get('MYSQLPORT', '3306'),
+        'NAME': 'biblio_smart_db',
+        'USER': '2hadRfj9ujbyhJw.root',
+        'PASSWORD': 'sGryvKTjGw8EHkvr',
+        'HOST': 'gateway01.eu-central-1.prod.aws.tidbcloud.com',
+        'PORT': '4000',
+        'OPTIONS': {
+            'ssl_mode': 'REQUIRED',
+            'charset': 'utf8mb4',
+        }
     }
 }
 
